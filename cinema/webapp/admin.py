26d9908c2ce1ff1e008540fc5d1a40daa4b2b07f
galubezzz <galubezzz@gmail.com>
2019-03-01 13:46:39 -0800

    register model in admin

--- cinema/webapp/admin.py
+++ cinema/webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'release_date']
+    ordering = ['-release_date']
+    search_fields = ['name', 'id']
+
+
+admin.site.register(Movie, MovieAdmin)
