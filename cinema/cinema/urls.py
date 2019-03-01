0927108270b9fe1ada4a46f7a4bfd7a649986fc7
galubezzz <galubezzz@gmail.com>
2019-03-01 14:30:44 -0800

    changes in urls

--- cinema/cinema/urls.py
+++ cinema/cinema/urls.py
@@ -18,12 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api_v1.urls', namespace='api_v1'))
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
