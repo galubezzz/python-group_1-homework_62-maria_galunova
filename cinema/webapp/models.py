ac3cfbf1f617b2d96a8dd839245457b3f735bb45
galubezzz <galubezzz@gmail.com>
2019-03-01 13:45:47 -0800

    add model for cinema

--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    poster = models.ImageField(upload_to='posters', null=True, blank=True)
+    release_date = models.DateField()
+    finish_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
