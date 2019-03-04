71650e69d3404e9332f1a50c0f5e3279a00d9486
galubezzz <galubezzz@gmail.com>
2019-03-03 20:47:47 -0800

    add viewsets for models

--- cinema/api_v1/views.py
+++ cinema/api_v1/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
-from webapp.models import Movie
+
+from webapp.models import Movie, Category, Show, Hall, Seat
 from rest_framework import viewsets
-from api_v1.serializers import MovieSerializer
+from api_v1.serializers import MovieSerializer, CategorySerializer, ShowSerializer, SeatSerializer, HallSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all().order_by('-release_date')
     serializer_class = MovieSerializer
 
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by("name")
+    serializer_class = CategorySerializer
+
+
+class ShowViewSet(viewsets.ModelViewSet):
+    queryset = Show.objects.all().order_by("movie")
+    serializer_class = ShowSerializer
+
+
+class HallViewSet(viewsets.ModelViewSet):
+    queryset = Hall.objects.all().order_by("name")
+    serializer_class = HallSerializer
+
+class SetViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all().order_by("hall")
+    serializer_class = SeatSerializer
