3a5aeaad0cb25f53f93b784e21dd9fed0495f298
galubezzz <galubezzz@gmail.com>
2019-03-05 15:36:25 -0800

    change in Movie serializer

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -1,31 +1,46 @@
 from webapp.models import Movie, Category, Show, Hall, Seat
 from rest_framework import serializers
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ('id', 'name', 'description', 'poster', 'release_date', 'finish_date')
-
 class CategorySerializer(serializers.ModelSerializer):
+
     url = serializers.HyperlinkedIdentityField(view_name='api_v1:category-detail')
+
     class Meta:
         model = Category
         fields= ("id", "name", "description")
+
 
 class InlineCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
 
+
+class MovieSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:movie-detail')
+    categories = InlineCategorySerializer(many=True, read_only=True)
+    category_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        write_only=True,
+        queryset=Category.objects.all(),
+        source='categories'
+    )
+    class Meta:
+        model = Movie
+        fields = ('id', 'name', 'description', 'poster', 'release_date', 'finish_date')
+
+
 class ShowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Show
         fields= ("id", "name", "hall", "start_time", "end_time", "price")
 
+
 class HallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hall
         fields= ("id", "name")
+
 
 class SeatSerializer(serializers.ModelSerializer):
     class Meta:
