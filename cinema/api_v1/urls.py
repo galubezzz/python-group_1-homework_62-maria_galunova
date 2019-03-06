8767d2fb918d8d446dca3fed8da0e733396c8270
galubezzz <galubezzz@gmail.com>
2019-03-05 18:16:23 -0800

    add urls for discount, ticket and booking

--- cinema/api_v1/urls.py
+++ cinema/api_v1/urls.py
@@ -8,6 +8,10 @@ router.register(r'category', views.CategoryViewSet)
 router.register(r'show', views.ShowViewSet)
 router.register(r'hall', views.HallViewSet)
 router.register(r'seat', views.SeatViewSet)
+router.register(r'discount', views.DiscountViewSet)
+router.register(r'ticket', views.TicketViewSet)
+router.register(r'booking', views.BookingViewSet)
+
 
 
 app_name = 'api_v1'
