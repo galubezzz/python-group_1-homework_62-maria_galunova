323b3ae0b112b3e1cdd0c3d5be03197562df11f0
galubezzz <galubezzz@gmail.com>
2019-03-13 23:37:03 -0700

    add filter for shows

--- frontend/src/containers/HallDetails/HallDetails.js
+++ frontend/src/containers/HallDetails/HallDetails.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import {NavLink} from 'react-router-dom';
+import Hall from "../../components/Hall/Hall";
 
 class HallDetails extends Component {
 
     state = {
-        hall: null
+        hall: null,
+        shows: null
     };
 
     componentDidMount() {
@@ -22,16 +24,33 @@ class HallDetails extends Component {
             })
             .then(hall => this.setState({hall}))
             .catch(error => console.log(error));
+
+        const SHOW_URL = "http://127.0.0.1:8000/api/v1/show/";
+        axios.get(SHOW_URL)
+            .then(response => {
+                console.log(response.data);
+                return response.data;
+            })
+            .then(shows =>
+            {   let showsInHall = shows.filter(show => show.hall.id == match.params.id);
+                this.setState({shows: showsInHall})})
+            .catch(error => console.log(error));
     }
 
     render() {
 
-        if (!this.state.hall) return null;
+        if (!this.state.hall || !this.state.shows) return null;
+
         return (
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">{this.state.hall.name}</h5>
                     <NavLink className="nav-link" to="/halls/">К списку залов</NavLink>
+                    {this.state.shows.map(show => {
+                    return <div key={show.id}>
+                            !!!{show.name.name}
+                    </div>
+                })}
                 </div>
             </div>
         )
