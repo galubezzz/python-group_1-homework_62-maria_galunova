76796a0e0dede4a7b7c21ac6154397903320b63e
galubezzz <galubezzz@gmail.com>
2019-03-22 15:19:10 -0700

    add User serializer

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -1,6 +1,21 @@
 from webapp.models import Movie, Category, Show, Hall, Seat, Discount, Ticket, Booking
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
 
 class InlineCategorySerializer(serializers.ModelSerializer):
     class Meta:
