6d6e065fbf1314af459e26ad0e64b3e0adbeda11
galubezzz <galubezzz@gmail.com>
2019-03-31 22:53:24 -0700

    install redux and created a storage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,23 @@ import * as serviceWorker from './serviceWorker';
 
 import "react-datepicker/dist/react-datepicker.css";
 
+import {createStore, applyMiddleware} from 'redux'
+import reducer from './store/reducer'
+import {Provider} from 'react-redux'
+import thunkMiddleware from 'redux-thunk';
+
+// TODO: убрать, как только все запросы "переедут" в actions.js
+import axios from 'axios';
+import {BASE_URL} from "./api-urls";
+axios.defaults.baseURL = BASE_URL;
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.getElementById('root')
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ frontend/src/store/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+     login: {
+
+      },
+     register: {
+
+      },
+     movieList: {
+
+      },
+     movieDetail: {
+
+      },
+     movieAdd: {
+
+      },
+     movieEdit: {
+
+      }
+ };
+
+  const reducer = (state = initialState, action) => {
+     switch (action.type) {
+         default:
+             return state;
+     }
+ };
+
+  export default reducer;
